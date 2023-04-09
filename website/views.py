17899411f10cec9d44799690d9a423e79e857081
ahmed hamza jandoubi <ahmedhamza0601@gmail.com>
2023-04-09 20:49:29 +0100

    add records

--- website/views.py
+++ website/views.py
@@ -2,9 +2,12 @@ from django.shortcuts import render,redirect
 from django.contrib.auth import authenticate , login,logout
 from django.contrib import messages
 from .forms import SignUpForm
+from .models import Record
 
 # Create your views here.
 def home(request):
+    records= Record.objects.all()
+
     if request.method =='POST':
         username=request.POST['username']
         password=request.POST['password']
@@ -17,7 +20,7 @@ def home(request):
             messages.success(request,'There was an error logging In , Please Try Again ...')
             return redirect('home')
     else:
-        return render(request,'home.html',{})
+        return render(request,'home.html',{'records':records})
 
 #def login_user(request):
    # pass
@@ -42,3 +45,11 @@ def register_user(request):
         form=SignUpForm()
         return render(request,'register.html',{'form':form})
     return render(request,'register.html',{'form':form})
+
+def customer_record(request,pk):
+    if request.user.is_authenticated:
+        customer_record=Record.objects.get(id=pk)
+        return render(request,'record.html',{'customer_record':customer_record})
+    else:
+        messages.success(request,"You Must be Logged In To view That Page.")
+        return redirect('home')
